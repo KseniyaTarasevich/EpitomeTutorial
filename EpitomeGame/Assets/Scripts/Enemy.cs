2943902e75ceb98dc02e00df2805e9375dc666e4
Kseniya Tarasevich <tarasevichkseniya11@gmail.com>
2022-02-01 20:14:25 +0300

    minor changes

--- EpitomeGame/Assets/Scripts/Enemy.cs
+++ EpitomeGame/Assets/Scripts/Enemy.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : Fighter
+public class Enemy : Mover
 {
     // experience
     public int xpValue = 1;
@@ -14,5 +14,35 @@ public class Enemy : Fighter
     private bool collidingWithPlayer;
     private Transform playerTransform;
     private Vector3 startingPosition;
+
+    // hitbox
+    private BoxCollider2D hitbox;
+    private Collider2D[] hits = new Collider2D[10];
+
+    protected override void Start()
+    {
+        base.Start();
+        playerTransform = GameManager.instance.player.transform;
+        startingPosition = transform.position;
+        hitbox = transform.GetChild(0).GetComponent<BoxCollider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        // is the player in range?
+
+        if (Vector3.Distance(playerTransform.position, startingPosition) < chaseLength)
+        {
+            chasing = Vector3.Distance(playerTransform.position, startingPosition) < triggerLength;
+
+            if (chasing)
+            {
+
+            }
+        }
+
+
+        UpdateMotor(Vector3.zero);
+    }
 
 }
